d7e1e54902092268b6df8e6845a8e4a1a52f074b
liliang <han-joy@Han-JoytekiMac-mini.local>
2013-03-12 17:21:17 +0800

    Initial Commit

---
+++ SanGuo_Layout/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SanGuo_LayoutAppController.h
+//  SanGuo_Layout
+//
+//  Created by liliang on 13-3-12.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SanGuo_Layout/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SanGuo_LayoutAppController.h
+//  SanGuo_Layout
+//
+//  Created by liliang on 13-3-12.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
